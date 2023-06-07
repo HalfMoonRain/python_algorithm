faabba28336f33b26e6cb67b35f8816acfccc0cf
junwoo <junwoo1357@gmail.com>
2023-06-07 10:10:50 +0900

    소수찾기 새 풀이

--- Level1/소수_찾기.py
+++ Level1/소수_찾기.py
@@ -25,7 +25,7 @@ n = 5
 # number = []
 # for i in range(2, n+1):
 #     number.append(i)
-#     if 
+#     if
 
 # 1번풀이. 속도에서 문제가 생김
 # def solution(n):
@@ -39,7 +39,7 @@ n = 5
 # def check_primary_number(n:int):
 #     divide_range = n//2
 #     for i in range(2, divide_range+1):
-#         if n % i == 0 : 
+#         if n % i == 0 :
 #             return False
 #     return True
 
@@ -51,9 +51,41 @@ def eratosthenes_sieve(n):
 
     for current_number in range(2, int(n**0.5) + 1):  # n의 제곱근까지만 확인하면 됩니다.
         if sieve[current_number]:  # 현재 수가 소수로 체크되어 있다면
-            for multiple in range(current_number**2, n+1, current_number):  # 그 수의 배수는 모두 소수가 아닌 것으로 체크합니다.
+            # 그 수의 배수는 모두 소수가 아닌 것으로 체크합니다.
+            for multiple in range(current_number**2, n+1, current_number):
                 sieve[multiple] = False
 
-    primes = [num for num in range(2, n+1) if sieve[num]]  # True로 남아 있는 수, 즉 소수들을 모아서 반환합니다.
+    # True로 남아 있는 수, 즉 소수들을 모아서 반환합니다.
+    primes = [num for num in range(2, n+1) if sieve[num]]
 
     return len(primes)
+
+# 다른 풀이
+
+
+def solution2(n):
+    # 2 ~ n 까지 set을 만듬
+    num = set(range(2, n+1))
+
+    # 2부터 값이 있으면 그거보다 큰 배수 들을 지워줌 (에라토스테네스의 체 원리)
+    for i in range(2, n+1):
+        if i in num:
+            # set으로 지워줌.
+            num -= set(range(2*i, n+1, i))
+
+    return len(num)
+
+# 활용 풀이
+
+
+def solution3(n):
+    num = set(range(2, n+1))
+
+    for i in range(2, int((n**0.5))+1):
+        if i in num:
+            num -= set(range(2*i, n+1, i))
+
+    return len(num)
+
+
+print(solution3(n))
